2f6d20a0574802a3ffd474e04161d86e398bed72
ayush.zerror@gmail.com <ayush.zerror@gmail.com>
2025-01-25 16:55:52 +0530

    first commit

---
+++ src/Components/Hero/Section1.jsx
@@ -0,0 +1,393 @@
+import gsap from 'gsap';
+import React, { useEffect, useState } from 'react'
+
+const Section1 = () => {
+
+  const [disabledName, setDisabledName] = useState(false)
+  const [disabledEmail, setDisabledEmail] = useState(true)
+  const [disabledcontact, setDisabledcontact] = useState(true)
+  const [disabledCity, setDisabledCity] = useState(true)
+  const [formData, setFormData] = useState({
+    name: '',
+    contact: '',
+    email: '',
+    city: '',
+  })
+  const handleName = (e) => {
+    e.preventDefault()
+    if (formData.name.trim().length > 0) {
+      setDisabledName(true)
+      document.querySelector(".name-btn").classList.remove("btn-hover")
+      gsap.to(".form2", {
+        opacity: 1,
+        duration: .5,
+        onComplete: () => {
+          setDisabledcontact(false)
+        }
+      })
+    }
+  }
+  const handleContact = (e) => {
+    e.preventDefault()
+    if (formData.contact.trim().length > 0) {
+      setDisabledcontact(true)
+      document.querySelector(".contact-btn").classList.remove("btn-hover")
+      gsap.to(".form3", {
+        opacity: 1,
+        duration: .5,
+        onComplete: () => {
+          setDisabledEmail(false)
+        }
+      })
+    }
+  }
+  const handleEmail = (e) => {
+    e.preventDefault()
+    if (formData.email.trim().length > 0) {
+      setDisabledEmail(true)
+      document.querySelector(".email-btn").classList.remove("btn-hover")
+      gsap.to(".form4", {
+        opacity: 1,
+        duration: 1,
+        onComplete: () => {
+          setDisabledCity(false)
+        }
+      })
+    }
+
+  }
+  const handleCity = (e) => {
+    e.preventDefault()
+    if (formData.city.trim().length > 0) {
+      setDisabledCity(true)
+      document.querySelector(".city-btn").classList.remove("btn-hover")
+
+      document.querySelector(".step-count").textContent = "3"
+      var tl = gsap.timeline()
+      .to(".form-container", {
+        opacity: 0,
+        duration: .5,
+        onComplete:()=>{
+          document.querySelector(".form-container").style.display = "none"
+        }
+      })
+      .to(".forms-pt2", {
+        onStart: () => {
+          document.querySelector(".form-container2").style.display = "flex"
+        },
+        opacity: 1,
+        transform: "translateY(0%)",
+        stagger: {
+          amount: .4,
+        },
+        duration: 1,
+        delay: .3
+      })
+      // .to(".forms-light2", {
+      //   opacity: .3,
+      //   duration: .5,
+      // })
+
+    }
+  }
+
+  const handleInfoHover = (e) => {
+    document.querySelector("#info-container").style.color = "#fff";
+    document.querySelector("#info-container").style.backgroundColor = "#393939";
+  }
+  const handleInfoLeave = (e) => {
+    document.querySelector("#info-container").style.color = "transparent";
+    document.querySelector("#info-container").style.backgroundColor = "transparent";
+  }
+
+  const handleClick = (e) => {
+    var tl = gsap.timeline()
+    tl
+      .to("#banner-content", {
+        y: -50,
+        opacity: 0,
+        duration: .5
+      }, "a")
+      .to(".corner", {
+        opacity: 0,
+        duration: .5
+      }, "a")
+      .to(".banner-card", {
+        onStart: function () {
+          gsap.set("#banner-card-container", { display: "flex" })
+        },
+        opacity: 1,
+        transform: "translateY(0%)",
+        stagger: {
+          amount: .3,
+        },
+        duration: 1.5,
+      })
+      .to(".card-content,#que1", {
+        opacity: 1,
+        duration: 1
+      })
+  }
+
+  useEffect(() => {
+    document.querySelectorAll(".banner-card").forEach(function (card) {
+      card.addEventListener("mouseenter", function () {
+        document.querySelectorAll(".banner-card").forEach(function (c) {
+          c.style.opacity = .5
+        })
+        card.style.opacity = 1
+      })
+      card.addEventListener("mouseleave", function () {
+        document.querySelectorAll(".banner-card").forEach(function (c) {
+          c.style.opacity = 1
+        })
+      })
+      card.addEventListener("click", function () {
+        document.querySelector(".step-count").textContent = "2"
+        var tl = gsap.timeline()
+        tl
+          .to("#cards", {
+            opacity: 0,
+            duration: .5
+          }, "a")
+          .to("#question1", {
+            opacity: 0,
+            duration: .5,
+            onComplete: () => {
+              document.querySelector("#subtitle").style.display = "block"
+            }
+          }, "a")
+          .to(".forms", {
+            onStart: () => {
+              document.querySelector(".form-container").style.display = "flex"
+            },
+            opacity: 1,
+            transform: "translateY(0%)",
+            stagger: {
+              amount: .4,
+            },
+            duration: 1,
+            delay: .3
+          })
+          .to(".input-box,#question2,#subtitle", {
+            opacity: 1,
+            duration: .5
+          })
+          .to(".forms-light", {
+            opacity: .3,
+            duration: .5,
+          })
+      })
+    })
+
+  }, [])
+
+  useEffect(()=>{
+    var tl2 = gsap.timeline()
+    tl2
+    .to(".hero-banner",{
+      transform:"translateY(0%)",
+      opacity:1,
+      duration:2,
+      delay:.5
+    })
+    .to(".home-section1",{
+      padding:"1vw",
+      duration:1,
+      ease:"power3.out",
+      delay:.2
+    },"s")
+    .to(".hero-banner",{
+      borderRadius:"20px",
+      opacity:1,
+      duration:2,
+      delay:.5
+    },"s")
+    .to(".corner,#banner-content,#info-container",{
+      opacity:1,
+      duration:2,
+      delay:.7
+    },"s")
+  },[])
+
+  return (
+    <div className='home-section1'>
+      <div className="hero-banner">
+        <div id='banner-content'>
+          <img src="/images/fdci.png" alt="" />
+          <p>Is back with another edition
+            and it's bigger than ever.</p>
+          <div onClick={handleClick} id='banner-btn'>
+            <h6>Register now</h6>
+            <span id='banner-btn-cir'>
+              <span id='icon-btn'><i className="ri-arrow-right-up-line"></i></span>
+            </span>
+          </div>
+        </div>
+        <div id='banner-btm'>
+          <div id='info-container'>
+            <span onMouseEnter={handleInfoHover} onMouseLeave={handleInfoLeave} id='info-icon'>
+              <i className="ri-info-i"></i>
+            </span>
+            <p>This website does not collect personal data and does not use cookies, as the settings are saved in your own browser.</p>
+          </div>
+        </div>
+        <img className='corner' id='corner1' src="/images/asset1.png" alt="" />
+        <img className='corner' id='corner2' src="/images/asset2.png" alt="" />
+        <div id='banner-card-container'>
+          <div id='que1'>
+            <p id='question1'>Are you the bride, groom, or a family member?</p>
+            <p id='question2'>The registration details for the FDCI Manifest Wedding Weekend 2025 .</p>
+            <p id='count'>0<span className='step-count'>1</span> <span id='que-count'> /03</span></p>
+          </div>
+          <p id='subtitle'>This step is one-time setup, but you can reset it on your dashboard.</p>
+
+          <div className='card-wrapper'>
+            <div id='cards'>
+              <div className="banner-card">
+                <div className='card-content'>
+                  <p>bride</p>
+                  <img src="/images/bride.png" alt="" />
+                  <h3>Explore my website with moderate animations, a customized dashboard, and more information about me.</h3>
+                </div>
+              </div>
+              <div className="banner-card">
+                <div className='card-content'>
+                  <p>groom</p>
+                  <img src="/images/groom.png" alt="" />
+                  <h3>Explore my website with moderate animations, a customized dashboard, and more information about me.</h3>
+                </div>
+              </div>
+              <div className="banner-card">
+                <div className='card-content'>
+                  <p>family</p>
+                  <img src="/images/family.png" alt="" />
+                  <h3>Explore my website with moderate animations, a customized dashboard, and more information about me.</h3>
+                </div>
+              </div>
+            </div>
+            <div className='form-container'>
+              <form action="" autoComplete='off' onSubmit={handleName} className='forms form1'>
+                <div className='input-container'>
+                  <div className='input-box'>
+                    <p>What's your name?</p>
+                    <div className='input-wrapper'>
+                      <input type="text" disabled={disabledName} name='name' onChange={(e) => setFormData({ ...formData, [e.target.name]: e.target.value })} value={formData.name} required maxLength="20" placeholder='Enter your first name' />
+                      <button className='done-btn name-btn btn-hover' disabled={disabledName}>
+                        <p>done</p>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </form>
+              <form action="" autoComplete='off' onSubmit={handleContact} className='forms forms-light form2'>
+                <div className='input-container'>
+                  <div className='input-box'>
+                    <p>Drop your contact number so we can stay in touch!</p>
+                    <div className='input-wrapper'>
+                      <input type="text" name='contact' disabled={disabledcontact} required onChange={(e) => setFormData({ ...formData, [e.target.name]: e.target.value })} value={formData.contact} placeholder='Enter your contact number' />
+                      <button className='done-btn contact-btn btn-hover' disabled={disabledcontact}>
+                        <p>done</p>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </form>
+              <form action="" autoComplete='off' onSubmit={handleEmail} className='forms forms-light form3'>
+                <div className='input-container'>
+                  <div className='input-box'>
+                    <p>Please enter your email address</p>
+                    <div className='input-wrapper'>
+                      <input type="email" placeholder='Enter your email ID' name='email' required disabled={disabledEmail} onChange={(e) => setFormData({ ...formData, [e.target.name]: e.target.value })} value={formData.email} />
+                      <button className='done-btn email-btn btn-hover' disabled={disabledEmail}>
+                        <p>done</p>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </form>
+              <form action="" autoComplete='off' onSubmit={handleCity} className='forms forms-light form4'>
+                <div className='input-container'>
+                  <div className='input-box'>
+                    <p>Where are you located?</p>
+                    <div className='input-wrapper'>
+                      <input type="text" disabled={disabledCity} required placeholder='Enter your city' name='city' onChange={(e) => setFormData({ ...formData, [e.target.name]: e.target.value })} value={formData.city
+
+                      } />
+                      <button className='done-btn city-btn btn-hover' disabled={disabledCity}>
+                        <p>done</p>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </div>
+            <div className='form-container2'>
+              <form action="" autoComplete='off' className='forms-pt2'>
+                <div className='input-container'>
+                  <div className='input-box2'>
+                    <p>Please enter your occupation.</p>
+                    <div className='input-wrapper'>
+                      <input type="text" name='name' placeholder='Enter your occupation' />
+                      <button className='done-btn btn-hover'>
+                        <p>done</p>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </form>
+              <form action="" autoComplete='off' className='forms-pt2 forms-light2 forms-pt21'>
+                <div className='input-container'>
+                  <div className='input-box2'>
+                    <p>What’s your Instagram handle?</p>
+                    <div className='input-wrapper'>
+                      <input type="text" name='name' placeholder='Enter your Instagram handle' />
+                      <button className='done-btn btn-hover'>
+                        <p>done</p>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </form>
+              <form action="" autoComplete='off' className='forms-pt2 forms-light2 forms-pt22'>
+                <div className='input-container'>
+                  <div className='input-box2'>
+                    <p>Are you or someone you know getting married?</p>
+                    <div className='input-wrapper'>
+                      <button className='done-btn btn-hover option-btn'>
+                        <p>If yes, please share the details!</p>
+                      </button>
+                      <button className='done-btn btn-hover option-btn'>
+                        <p>No</p>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </form>
+              <form action="" autoComplete='off' className='forms-pt2 forms-light2 forms-pt23'>
+                <div className='input-container'>
+                  <div className='input-box2'>
+                    <p>What excites you the most when it comes to weddings?</p>
+                    <div className='input-wrapper'>
+                      <button className='done-btn btn-hover option-btn'>
+                        <p>Wedding Wear Designers</p>
+                      </button>
+                      <button className='done-btn btn-hover option-btn'>
+                        <p>Fine Jewellery</p>
+                      </button>
+                      <button className='done-btn btn-hover option-btn'>
+                        <p>Trousseau & Wedding Services</p>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Section1
---
+++ src/Components/Hero/Section2.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const Section2 = () => {
+  return (
+    <div id='home-section2'>
+      <h2>FDCI Manifest Wedding Weekend</h2>
+      <div id='date-container'>
+        <div className='text-wrap-d'>
+          <i className="ri-calendar-2-line"></i>
+          <p>3 August, 2024 - 4 August, 2024
+          </p>
+        </div>
+        <div className='text-wrap-d'>
+          <i className="ri-map-pin-line"></i>
+          <p>Taj Palace, New Delhi</p>
+        </div>
+      </div>
+      <div id="about-text-container">
+        <p>Experience the Ultimate in Wedding Luxury at the FDCI Manifest Wedding Weekend</p>
+        <p>New Delhi, June 10th, 2024: The Fashion Design Council of India (FDCI) and RPSG Lifestyle Media are ecstatic to jointly announce the FDCI Manifest Wedding Weekend, which will take place at the Taj Palace, New Delhi, on August 3rd and 4th , 2024.</p>
+        <p>An ultra-luxurious showcase of the best in couture, trends and jewellery, the FDCI Manifest Wedding Weekend will consist of a bespoke curation of India’s top couturiers, jewellers and ancillary service providers to give to-be brides, grooms and their families a one-stop experience under one roof.</p>
+        <p>It will also mark the launch of RPSG Lifestyle Media’s wedding vertical, Manifest, that will consist of a magazine, digital engagements, online experiences, and more. The group already publishes celebrity magazine HELLO! India, and is soon to launch Indian editions of Esquire and The Hollywood Reporter.</p>
+        <p>The event will kick off on the day after FDCI’s annual Hyundai India Couture Week, which is a highly anticipated event on the city’s cultural and fashion calendar.</p>
+        <p>The FDCI Wedding Weekend aims at presenting the newest wedding trends by face-to-face interactions between patrons and the designers themselves. The selection of those showcasing their creations will be curated by FDCI and the editorial team of Manifest, ensuring a rich experience for those attending. Given the personal nature of interactions involved with creative professionals, the event will be access-regulated, and patrons will be encouraged to attend by prior appointment and invitation only.</p>
+        <p>Reflecting on the event’s significance, FDCI Chairman Sunil Sethi shared, “The overwhelming response to our inaugural FDCI Wedding Weekend set a high standard. This year, we aim to elevate the shopping experience even more. Print and digital media together form a comprehensive and creative promotional strategy, reaching diverse audiences and driving consumer interest. By uniting coveted couturiers, jewellers, and wedding professionals, we continue to tap into the booming Indian wedding and luxury fashion market, making this event the premier platform for acquiring festive couture, red-carpet creations, and exquisite jewellery.”</p>
+        <p>Avarna Jain, Chairperson of RPSG Lifestyle Media, added, “We are delighted to launch our first homegrown brand at this exclusive event in association with the Fashion Design Council of India. The FDCI Manifest Wedding Weekend will not just bring together India’s top couturiers and the most celebrated jewellers for a bespoke two-day showcase but it will also set the trends for the season. Soon to be brides and grooms, and their families, painstakingly curated by our audience development teams, will be able to interact with India’s best couturiers and jewellers. This sets the tone for our couture, luxury and wedding initiative, that will reinvent the way young India looks at celebration and joy.”</p>
+        <p>The Fashion Design Council of India (FDCI) is the country’s premiere representational body of fashion professionals, which encourages Indian artisans and helps develop young talent.</p><p>RPSG Lifestyle Media is a part of the USD 7-billion RP-Sanjiv Goenka Group with interests in power, carbon black, IT, retail, FMCG, sports and entertainment.</p>
+      </div>
+      <div id='banner-btn' className='banner-btn-bottom'>
+        <h6>Register now</h6>
+        <span id='banner-btn-cir'>
+          <span id='icon-btn'><i className="ri-arrow-right-up-line"></i></span>
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export default Section2
